d7b39712a5a92db6fb4a63de3d5864e2517d71f1
piotte13 <jeremie.piotte@gmail.com>
2017-03-17 21:33:24 -0400

    mobile fix

--- src/app/player.ts
+++ src/app/player.ts
@@ -22,10 +22,19 @@ export class Player {
   }
 
   public setDickLength(){
-    let maxLen = 103;
-    if(window.innerWidth < 700) maxLen = 50;
-    else if(window.innerWidth < 990) maxLen = 58;
-    else if(window.innerWidth < 1200) maxLen = 80;
+    let maxLen: number;
+
+    switch(true){
+      case (window.innerWidth < 400): maxLen = 15; break;
+      case (window.innerWidth < 500): maxLen = 25; break;
+      case (window.innerWidth < 600): maxLen = 30; break;
+      case (window.innerWidth < 650): maxLen = 40; break;
+      case (window.innerWidth < 700): maxLen = 50; break;
+      case (window.innerWidth < 990): maxLen = 58; break;
+      case (window.innerWidth < 1200): maxLen = 80; break;
+      default: maxLen = 103;
+    }
+
     let score = _.random(1, maxLen);
 
     this._dick = "8";
