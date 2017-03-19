8b3e3c9f94ad72997df774429ec14fd7f706f54e
piotte13 <jeremie.piotte@gmail.com>
2017-03-18 20:28:06 -0400

    changement dans le css et changement de titre

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-  title = " Wolves of Peel Street";
+  title = "RollTheD.com";
 
   constructor() { }
 
